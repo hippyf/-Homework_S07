090b5691d123a5e9748371ea0ae84083e2c1b1da
yana sarvarova <serg.yana@mail.ru>
2022-07-25 23:29:18 +0300

    Задача 64

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все натуральные
+числа кратные 3-м в промежутке от M до N
+M = 1; N = 9 -- > "3, 6, 9" */
+
+int a = 1;
+int b = 9;
+Recursion (a, b);
+Console.WriteLine();
+
+void Recursion (int a, int b )
+{
+    if (a > b) return;
+    else if (a%3 == 0) Console.Write (a + " ");
+    a++;
+    Recursion (a, b);
+}
