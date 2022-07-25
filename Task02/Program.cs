d40da5ebd8de551d2de1e3e5a59f38682fcd7b59
yana sarvarova <serg.yana@mail.ru>
2022-07-25 23:43:58 +0300

    Задача 66

---
+++ Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N
+M = 1; N = 15 -- > 120 */
+
+int a = 1;
+int b = 15;
+Console.Write("Сумма натуральных элементов в промежутке от " + a);
+Recursion(a, b);
+Console.WriteLine();
+
+void Recursion(int a, int b, int sum = 0)
+{
+    if (a > b)
+    {
+        Console.Write(" до " + b + " равна " + sum);
+        return;
+    }
+    sum += a;
+    a++;
+    Recursion(a, b, sum);
+}
